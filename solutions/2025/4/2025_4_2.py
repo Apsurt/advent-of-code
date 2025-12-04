18285e528e4728d531185dad7e7e5975c6d71263
Apsurt <tymon@becella.eu>
2025-12-04 11:05:38 +0100

    feat: Solve 2025 Day 4 Part 2

---
+++ solutions/2025/4/2025_4_2.py
@@ -0,0 +1,41 @@
+import aoc
+
+# Set your Advent of Code context
+YEAR, DAY, PART = (2025, 4, 2)
+
+def solve():
+    # Solve the puzzle here
+    grid = aoc.Input().grid()
+    answer = 0
+
+    while True:
+        to_remove = []
+        for y in range(grid.height):
+            for x in range(grid.width):
+                if grid.get(y,x) != "@":
+                    continue
+                neighbours = grid.neighbors(y,x, diagonals=True).values()
+                count = 0
+                for cell in neighbours:
+                    if cell == "@":
+                        count += 1
+                    if count >= 4:
+                        break
+                if count < 4:
+                    to_remove.append((y,x))
+                    answer += 1
+        if len(to_remove) == 0:
+            break
+        for y,x in to_remove:
+            grid.data[y][x] = "."
+
+    return answer
+
+def main():
+    answer = solve()
+    print(answer)
+    # Uncomment line below when you're ready to submit
+    aoc.submit(answer)
+
+if __name__ == "__main__":
+    main()
